2dbaddaef6b722ded88ba066d11d0b5516801608
Lior Shub <liorbentov@gmail.com>
2018-01-27 11:51:54 +0200

    Add prettier

--- src/Components/ItemTypes.js
+++ src/Components/ItemTypes.js
@@ -1,3 +1,3 @@
 export default {
-    ROLE: 'role',
+	ROLE: 'role'
 };
--- src/Components/Role/Role.js
+++ src/Components/Role/Role.js
@@ -1,55 +1,46 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { DragSource } from 'react-dnd'
-import ItemTypes from '../ItemTypes'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { DragSource } from 'react-dnd';
+import ItemTypes from '../ItemTypes';
 
 const style = {
-    position: 'absolute',
-    border: '1px dashed',
-    borderColor: 'gray',
-    backgroundColor: 'white',
-    padding: '0.5rem 1rem',
-    cursor: 'move',
+	position: 'absolute',
+	border: '1px dashed',
+	borderColor: 'gray',
+	backgroundColor: 'white',
+	padding: '0.5rem 1rem',
+	cursor: 'move'
 };
 
 const roleSource = {
-    beginDrag(props) {
-        const { id, left, top } = props
-        return { id, left, top }
-    },
+	beginDrag(props) {
+		const { id, left, top } = props;
+		return { id, left, top };
+	}
 };
 
 class Role extends Component {
-    static propTypes = {
-        connectDragSource: PropTypes.func.isRequired,
-        isDragging: PropTypes.bool.isRequired,
-        id: PropTypes.any.isRequired,
-        left: PropTypes.number.isRequired,
-        top: PropTypes.number.isRequired,
-        hideSourceOnDrag: PropTypes.bool.isRequired,
-        children: PropTypes.node,
-    };
+	static propTypes = {
+		connectDragSource: PropTypes.func.isRequired,
+		isDragging: PropTypes.bool.isRequired,
+		id: PropTypes.any.isRequired,
+		left: PropTypes.number.isRequired,
+		top: PropTypes.number.isRequired,
+		hideSourceOnDrag: PropTypes.bool.isRequired,
+		children: PropTypes.node
+	};
 
-    render() {
-        const {
-            hideSourceOnDrag,
-            left,
-            top,
-            connectDragSource,
-            isDragging,
-            children,
-        } = this.props;
-        if (isDragging && hideSourceOnDrag) {
-            return null
-        }
+	render() {
+		const { hideSourceOnDrag, left, top, connectDragSource, isDragging, children } = this.props;
+		if (isDragging && hideSourceOnDrag) {
+			return null;
+		}
 
-        return connectDragSource(
-            <div style={{ ...style, left, top }}>{children}</div>,
-        )
-    }
+		return connectDragSource(<div style={{ ...style, left, top }}>{children}</div>);
+	}
 }
 
 export default DragSource(ItemTypes.ROLE, roleSource, (connect, monitor) => ({
-    connectDragSource: connect.dragSource(),
-    isDragging: monitor.isDragging(),
+	connectDragSource: connect.dragSource(),
+	isDragging: monitor.isDragging()
 }))(Role);
